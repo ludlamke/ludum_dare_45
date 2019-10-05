ef9c4511973ec92b2112dd961c29006de2f8f3af
Kevin Ludlam <kevinlud92@gmail.com>
2019-10-04 22:16:07 -0400

    10 break update

---
+++ ludum_dare_45/Assets/scripts/Camera_Controler.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Controler : MonoBehaviour
+{
+    public bool cameraEnabled;
+    private float dampTime = 0.15f;
+    private Vector3 velocity = Vector3.zero;
+    private Transform target;
+
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+   
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(cameraEnabled)
+        {
+        if (target)
+        {
+            Vector3 point = GetComponent<Camera>().WorldToViewportPoint(target.position);
+            Vector3 delta = target.position - GetComponent<Camera>().ViewportToWorldPoint(new Vector3(0.50f, 0.50f, point.z)); 
+            Vector3 destination = transform.position + delta;
+
+            transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+        }
+        }
+        
+    }
+}
+
---
+++ ludum_dare_45/Assets/scripts/Player_controler.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_controler : MonoBehaviour
+{
+    [SerializeField]
+    private float playerSpeed = 5;
+    [SerializeField]
+    private float playerJumpHight = 5;
+
+    
+    private Rigidbody2D PlayerRB;
+    private float playerVelocity;
+
+    private bool isGrounded;
+    private float GroindCheak = 0.1f;
+    private LayerMask ground;
+    private Transform feet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerRB = GetComponent<Rigidbody2D>();
+        feet = transform.GetChild(0);
+        ground = LayerMask.GetMask("Ground");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics2D.OverlapCircle(feet.position, GroindCheak, ground);
+        player_Move();
+        if(isGrounded && Input.GetButtonDown("Jump"))
+        {
+            PlayerJump();
+        }
+    }
+
+    void player_Move()
+    {
+
+        playerVelocity = playerSpeed * Input.GetAxisRaw("Horizontal");
+        PlayerRB.velocity = new Vector2(playerVelocity, PlayerRB.velocity.y);
+
+        if(PlayerRB.velocity.x > 0)
+        {
+            transform.localScale = new Vector2(this.transform.localScale.x, this.transform.localScale.y);
+            
+        }
+        else if(PlayerRB.velocity.x < 0)
+        {
+            transform.localScale = new Vector2(-this.transform.localScale.x, this.transform.localScale.y);
+        }
+    }
+
+    void PlayerJump()
+    {
+        PlayerRB.velocity = new Vector2(PlayerRB.velocity.x, playerJumpHight);
+    }
+}
